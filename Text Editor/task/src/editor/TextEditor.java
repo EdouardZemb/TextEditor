0fec687ba0f1a6a54e94836553f472759d7676c3
Edouard Zemb <edouard.zemb@gmail.com>
2023-05-14 14:17:22 +0200

    Refactor: Eliminate magic numbers in TextEditor class
    Replace hard-coded values with constants in the TextEditor class to improve code clarity and maintainability. Specifically, replace magic numbers used for the frame size, component names, and text area margin with constant values.

--- Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/task/src/editor/TextEditor.java
@@ -7,11 +7,17 @@ import java.awt.*;
 public class TextEditor extends JFrame {
     private static final int FRAME_WIDTH = 500;
     private static final int FRAME_HEIGHT = 500;
+    private static final String FRAME_NAME = "TextEditorFrame";
+    private static final String FRAME_TITLE = "Text Editor";
+    private static final String TEXT_AREA_NAME = "TextArea";
+    private static final String TEXT_AREA_SCROLL_PANE_NAME = "TextAreaScrollPane";
+    private static final int TEXT_AREA_MARGIN = 10;
 
     public TextEditor() {
+        setName(FRAME_NAME);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        setTitle("Text Editor");
+        setTitle(FRAME_TITLE);
 
         add(createTextAreaScrollPane(), BorderLayout.CENTER);
 
@@ -21,13 +27,11 @@ public class TextEditor extends JFrame {
     private JScrollPane createTextAreaScrollPane() {
         JTextArea textArea = createTextArea();
         JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setName("TextAreaScrollPane");
+        scrollPane.setName(TEXT_AREA_SCROLL_PANE_NAME);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-        // Add margin to the text area
-        int margin = 10;
-        textArea.setBorder(new EmptyBorder(margin, margin, margin, margin));
+        textArea.setBorder(new EmptyBorder(TEXT_AREA_MARGIN, TEXT_AREA_MARGIN, TEXT_AREA_MARGIN, TEXT_AREA_MARGIN));
 
         scrollPane.setVisible(true);
         return scrollPane;
@@ -35,7 +39,7 @@ public class TextEditor extends JFrame {
 
     private JTextArea createTextArea() {
         JTextArea textArea = new JTextArea();
-        textArea.setName("TextArea");
+        textArea.setName(TEXT_AREA_NAME);
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
         return textArea;
