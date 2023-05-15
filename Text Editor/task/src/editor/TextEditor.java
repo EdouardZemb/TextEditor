d151efb3ec53d07427efe28f304fec018db1ed8c
Edouard Zemb <edouard.zemb@gmail.com>
2023-05-15 08:38:57 +0200

    Refactor TextEditor class into smaller functions for improved code organization and readability

--- Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/task/src/editor/TextEditor.java
@@ -12,18 +12,40 @@ public class TextEditor extends JFrame {
     private static final int TEXT_AREA_MARGIN = 10;
 
     public TextEditor() {
+        configureFrame();
+        addContentPanel();
+        setVisible(true);
+    }
+
+    private void configureFrame() {
         setName(FRAME_NAME);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(FRAME_WIDTH, FRAME_HEIGHT);
         setTitle(FRAME_TITLE);
+    }
 
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.setBorder(new EmptyBorder(TEXT_AREA_MARGIN, TEXT_AREA_MARGIN, TEXT_AREA_MARGIN, TEXT_AREA_MARGIN));
-        panel.add(new FilenamePanel(), BorderLayout.NORTH);
-        panel.add(new TextAreaPanel(), BorderLayout.CENTER);
-
+    private void addContentPanel() {
+        JPanel panel = createContentPanel();
         getContentPane().add(panel, BorderLayout.CENTER);
+    }
 
-        setVisible(true);
+    private JPanel createContentPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+        configureContentPanel(panel);
+        addFilenamePanel(panel);
+        addTextAreaPanel(panel);
+        return panel;
+    }
+
+    private void configureContentPanel(JPanel panel) {
+        panel.setBorder(new EmptyBorder(TEXT_AREA_MARGIN, TEXT_AREA_MARGIN, TEXT_AREA_MARGIN, TEXT_AREA_MARGIN));
+    }
+
+    private void addFilenamePanel(JPanel panel) {
+        panel.add(new FilenamePanel(), BorderLayout.NORTH);
+    }
+
+    private void addTextAreaPanel(JPanel panel) {
+        panel.add(new TextAreaPanel(), BorderLayout.CENTER);
     }
 }
