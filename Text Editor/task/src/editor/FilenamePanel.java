8a80f1ad670d83ecddc7d4a31c8b333ce4823feb
Edouard Zemb <edouard.zemb@gmail.com>
2023-05-15 08:36:54 +0200

    Refactor FilenamePanel class into smaller functions for improved code organization and readability

--- Text Editor/task/src/editor/FilenamePanel.java
+++ Text Editor/task/src/editor/FilenamePanel.java
@@ -18,14 +18,24 @@ public class FilenamePanel extends JPanel {
         setLayout(new BorderLayout());
         setBorder(new EmptyBorder(PANEL_MARGIN, PANEL_MARGIN, PANEL_MARGIN, PANEL_MARGIN));
 
-        JLabel filenameLabel = new JLabel(FilenamePanel.LABEL_TEXT);
-        filenameLabel.setName(FilenamePanel.LABEL_NAME);
-        add(filenameLabel, BorderLayout.WEST);
+        addFilenameLabel();
+        addFilenameField();
+        addFilenameButtonPanel();
+    }
 
+    private void addFilenameLabel() {
+        JLabel filenameLabel = new JLabel(LABEL_TEXT);
+        filenameLabel.setName(LABEL_NAME);
+        add(filenameLabel, BorderLayout.WEST);
+    }
+
+    private void addFilenameField() {
         JTextField filenameField = new JTextField();
         filenameField.setName(TEXT_FIELD_NAME);
         add(filenameField, BorderLayout.CENTER);
+    }
 
+    private void addFilenameButtonPanel() {
         JPanel buttonPanel = createFilenameButtonPanel();
         add(buttonPanel, BorderLayout.EAST);
     }
@@ -33,14 +43,24 @@ public class FilenamePanel extends JPanel {
     private JPanel createFilenameButtonPanel() {
         JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
 
-        JButton saveButton = new JButton(FilenamePanel.SAVE_BUTTON_TEXT);
-        saveButton.setName(FilenamePanel.SAVE_BUTTON_NAME);
+        JButton saveButton = createSaveButton();
         buttonPanel.add(saveButton);
 
-        JButton loadButton = new JButton(FilenamePanel.LOAD_BUTTON_TEXT);
-        loadButton.setName(FilenamePanel.LOAD_BUTTON_NAME);
+        JButton loadButton = createLoadButton();
         buttonPanel.add(loadButton);
 
         return buttonPanel;
+    }
+
+    private JButton createSaveButton() {
+        JButton saveButton = new JButton(SAVE_BUTTON_TEXT);
+        saveButton.setName(SAVE_BUTTON_NAME);
+        return saveButton;
+    }
+
+    private JButton createLoadButton() {
+        JButton loadButton = new JButton(LOAD_BUTTON_TEXT);
+        loadButton.setName(LOAD_BUTTON_NAME);
+        return loadButton;
     }
 }
