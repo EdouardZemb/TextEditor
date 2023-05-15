8a18916f6f8a13b22c6d043ecb7c7aa07b235b67
Edouard Zemb <edouard.zemb@gmail.com>
2023-05-15 08:33:49 +0200

    Refactor TextAreaPanel class into smaller functions for improved code organization and readability

--- Text Editor/task/src/editor/TextAreaPanel.java
+++ Text Editor/task/src/editor/TextAreaPanel.java
@@ -13,21 +13,33 @@ public class TextAreaPanel extends JPanel {
         setLayout(new BorderLayout());
 
         JTextArea textArea = createTextArea();
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setName(TEXT_AREA_SCROLL_PANE_NAME);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-
-        textArea.setBorder(new EmptyBorder(TEXT_AREA_MARGIN, TEXT_AREA_MARGIN, TEXT_AREA_MARGIN, TEXT_AREA_MARGIN));
+        JScrollPane scrollPane = createScrollPane(textArea);
 
         add(scrollPane, BorderLayout.CENTER);
     }
 
     private JTextArea createTextArea() {
         JTextArea textArea = new JTextArea();
+        configureTextArea(textArea);
+        return textArea;
+    }
+
+    private void configureTextArea(JTextArea textArea) {
         textArea.setName(TEXT_AREA_NAME);
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
-        return textArea;
+        textArea.setBorder(new EmptyBorder(TEXT_AREA_MARGIN, TEXT_AREA_MARGIN, TEXT_AREA_MARGIN, TEXT_AREA_MARGIN));
+    }
+
+    private JScrollPane createScrollPane(Component view) {
+        JScrollPane scrollPane = new JScrollPane(view);
+        configureScrollPane(scrollPane);
+        return scrollPane;
+    }
+
+    private void configureScrollPane(JScrollPane scrollPane) {
+        scrollPane.setName(TEXT_AREA_SCROLL_PANE_NAME);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
     }
 }
